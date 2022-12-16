a3365ac2b166a71fcc1c27062ce7dd9cccea945e
Lethrial <maxkoopakop@gmail.com>
2022-12-16 23:16:23 +0000

    Fixed jumping bug
    Fixed bug where missing during the jumping cutscene would cause bf to reappear

--- data/loves-end/loves-end.lua
+++ data/loves-end/loves-end.lua
@@ -8,6 +8,7 @@ end
 function onCreatePost()
     bf_x = getProperty('boyfriend.x')
 	bf_y = getProperty('boyfriend.y') --using defaultBoyfriendX/Y didn't work for some reason
+	jumping = false
 
     --shock effect (thank you SAKK for making the sprite)
 	makeLuaSprite('shock', 'shock', bf_x - 130, bf_y - 120)
@@ -32,16 +33,22 @@ function onCountdownTick(counter)
 end
 
 function onUpdate(elapsed)
-	--why did you flip the skid animation funk mix team ):
-	if getProperty('boyfriend.animation.curAnim.name') == 'skid' then
-		setProperty('boyfriend.flipX', true)
-		setProperty('boyfriend.x', bf_x + 18)
-	else
-		setProperty('boyfriend.flipX', false)
-		setProperty('boyfriend.x', bf_x)
-	end
+	if not inGameOver then
+		if getProperty('boyfriend.animation.curAnim.name') == 'skid' then
+			setProperty('boyfriend.flipX', true)
+			setProperty('boyfriend.x', bf_x + 18)
+		else
+			setProperty('boyfriend.flipX', false)
+			setProperty('boyfriend.x', bf_x)
+		end
 
-	if curBeat < 340 then setProperty('dad.x', getProperty('dad.x') + (3 * elapsed)) end --moves imario every frame. multiplying by elapsed so the speed is constant on any frame rate   
+		if jumping then		
+			setProperty('boyfriend.y', -10000) -- getting a miss while jumping would cause bf to reappear so i just shoved him to the abyss instead
+			setProperty('boyfriend.visible', false)
+		end
+
+		if curBeat < 340 then setProperty('dad.x', getProperty('dad.x') + (3 * elapsed)) end --moves imario every frame. multiplying by elapsed so the speed is constant on any frame rate
+	end
 end
 
 function onEvent(name, value1, value2)
@@ -53,15 +60,13 @@ end
 function onBeatHit()
 	--suicide time!
 	if curBeat == 336 then characterDance('boyfriend') end
-	if curBeat > 337 then
+	if curBeat == 338 then
+		jumping = true
 		if getProperty('boyfriend.curCharacter') == 'bf-fire' then
 			objectPlayAnimation('jump', 'fire_jump', true)
 		else
 			objectPlayAnimation('jump', 'normal_jump', true)
 		end
-	end
-	if curBeat == 338 then
-		setProperty('boyfriend.visible', false)
 		setProperty('jump.visible', true)
 		doTweenX('jumpX', 'jump', bf_x - 270, 0.8, 'linear')
 		doTweenY('jumpYup', 'jump', bf_y - 220, 0.3, 'circOut')
@@ -80,6 +85,8 @@ function onGameOverStart()
     screenCenter('boyfriend')
     setProperty('boyfriend.x', getProperty('boyfriend.x') + 30)
     setProperty('boyfriend.y', getProperty('boyfriend.y') - 10)
+	setProperty('boyfriend.flipX', false)
+	setProperty('boyfriend.visible', true)
     playSound('death', 1, 'death')
     return Function_Continue
 end
